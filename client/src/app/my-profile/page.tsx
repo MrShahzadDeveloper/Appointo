748de789e2811a9fbd00ceafe5b60ba66393b714
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2025-10-28 23:26:32 +0500

    My Profile page is Setuped

--- client/src/app/my-profile/page.tsx
+++ client/src/app/my-profile/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import { assets } from "@/assets/assets_frontend/assets";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "Mr. Shahzad",
+    image: assets.profile_pic,
+    email: "mr.shahzad.developer@gmail.com",
+    phone: "+92 3415278601",
+    address: {
+      line1: "57th Cross, Richmond",
+      line2: "Circle, Brench Road, London",
+    },
+    gender: "Male",
+    dob: "2004-04-14",
+  });
 
-export default page
+  const [edit, setEdit] = useState(true);
+
+  return (
+    <div>
+      <Image src={userData.image} alt="User Image" />
+      <h1>{userData.name}</h1>
+      <hr />
+      <div>
+        <h1>Contact Information</h1>
+        <div>
+          <p>Name: </p>
+          {/* {
+          edit?
+          <input type="text" value={userData.name} onChange={e => setUserData(...prev ,name:e.target.value)} />
+         } */}
+          <p>{userData.name}</p>
+        </div>
+        <div>
+          <p>Phone: </p>
+          <p>{userData.phone}</p>
+        </div>
+        <div>
+          <p>Adress: </p>
+          <div>
+            <p>{userData.address.line1}</p>
+            <p>{userData.address.line2}</p>
+          </div>
+        </div>
+        <p>Basic Information</p>
+         <div>
+          <p>Gender: </p>
+          <p>{userData.gender}</p>
+        </div>
+         <div>
+          <p>Birthday: </p>
+          <p>{userData.dob}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
