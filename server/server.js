eb78525b5c09b231acf307c3c2992047a0c625fb
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2025-10-31 23:09:53 +0500

    Backend Folder is Setuped and Ready to Go

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-console.log("Hello this is Backend")
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+// app config
+const app = express()
+const port = process.env.PORT || 8000
+
+// middlewares
+app.use(express.json())
+app.use(cors()) // used for the connection of Frontend with server
+
+// api endpoint
+app.get("/",(req, res)=>{
+    res.send("Api Working Successfully")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is Running on Port ${port}`)
+})
